0dbd5b79dc8a8a196c1c97826eb2ab1463328fcd
Vitaly <itassan6@gmail.com>
2023-04-11 13:17:51 +0300

    downgrading react to 16.9.0 (to support react-codemirror2)

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import "./scss/index.scss";
-import App from "./App";
-import { Provider } from "react-redux";
-import { store } from "./store/store";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import './scss/index.scss';
+import App from './App';
+import { Provider } from 'react-redux';
+import { store } from './store/store';
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </BrowserRouter>,
+  document.getElementById('root')
 );
